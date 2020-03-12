4d45dac95acd1774e5d5c741377e92c30a87f69b
Postamentovich <postamentovich@gmail.com>
2020-03-12 21:48:26 +0300

    add

---
+++ promisify.js
@@ -0,0 +1,17 @@
+export default function promisify(f) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      function callback(err, result) {
+        if (err) {
+          return reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback);
+
+      f.call(this, ...args);
+    });
+  };
+}
