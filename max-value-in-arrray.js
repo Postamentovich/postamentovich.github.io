ddd079377b1e04ad49de281a1bcfe3fe3b7055e7
Postamentovich <postamentovich@gmail.com>
2020-03-13 21:00:27 +0300

    update

---
+++ max-value-in-arrray.js
@@ -0,0 +1,137 @@
+const {
+  AsyncArray,
+  add,
+  subtract,
+  multiply,
+  divide,
+  mod,
+  less,
+  equal,
+  lessOrEqual,
+  sqrt
+} = Homework;
+
+/**
+ * Промисификация функций с callback
+ * @param {func} f 
+ * @returns {() => Promise}
+ */
+function promisify(f) {
+  return function(...args) {
+    return new Promise(resolve => {
+      function callback(result) {
+        resolve(result);
+      }
+      args.push(callback);
+      f.call(this, ...args);
+    });
+  };
+}
+
+
+const array1 = new AsyncArray([-500, 1, 2, 100, 5, 7, 10, 10000]);
+
+const array2 = new AsyncArray([-900, 800, 1, 0, -9]);
+
+const promisifyAdd = promisify(add);
+const promisifyLess = promisify(less);
+const promisifyEqual = promisify(equal);
+
+/**
+ * Функция находит максимальный элемент в массиве.
+ * Реализована с помощью async/await.
+ *
+ * @param {AsyncArray<number>} array - асинхронный массив
+ * @param {(result: number | null) => void} cb - callback
+ */
+async function getMaxValueWithAwait(array, cb) {
+  const promisifyLength = promisify(array.length);
+  const promisifyGet = promisify(array.get);
+
+  let maxValue = null;
+
+  const length = await promisifyLength();
+
+  /**
+   * Проходим по массиву циклом for, и проверяем, меньше
+   * текущее максимальное значение чем элемент массива или нет.
+   */
+
+  for (
+    let i = 0;
+    await promisifyLess(i, length);
+    i = await promisifyAdd(i, 1)
+  ) {
+    const item = await promisifyGet(i);
+    if (await promisifyEqual(maxValue, null)) maxValue = item;
+    else if (await promisifyLess(maxValue, item)) maxValue = item;
+  }
+
+  cb(maxValue);
+}
+
+/**
+ * Функция находит максимальный элемент в массиве.
+ * Реализована с помощью Promise.then().
+ *
+ * @param {AsyncArray<number>} array - асинхронный массив
+ * @param {(result: number | null) => void} cb - callback
+ */
+function getMaxValueWithThen(array, cb) {
+  const promisifyPop = promisify(array.pop);
+
+  let maxValue = null;
+
+  /**
+   * С помощью рекурсии забираем последний элемент мaссива, и проверяем, меньше
+   * текущее максимальное значение чем элемент массива или нет.
+   */
+
+  const checkLastElement = () => {
+    let lastElement = null;
+
+    promisifyPop()
+      /** Получем элемент */
+      .then(item => {
+        lastElement = item;
+
+        return promisifyEqual(typeof item, "number");
+      })
+      /** Проверяем есть элемент или нет */
+      .then(isExist => {
+        if (!isExist) {
+          cb(maxValue);
+          return Promise.reject("Element don't exist, or not a number");
+        }
+
+        return promisifyEqual(maxValue, null);
+      })
+      /** Проверяем текущее максимально значение на null */
+      .then(isNull => {
+        if (isNull) {
+          maxValue = lastElement;
+          checkLastElement();
+          return Promise.reject("maxValue is a null");
+        }
+
+        return promisifyLess(maxValue, lastElement);
+      })
+      /** Сравниваем значение элемента и максимальное значение */
+      .then(isLess => {
+        if (isLess) {
+          maxValue = lastElement;
+        }
+
+        checkLastElement();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  checkLastElement();
+}
+
+getMaxValueWithThen(array1, result => console.log(result));
+
+getMaxValueWithAwait(array2, result => console.log(result));
