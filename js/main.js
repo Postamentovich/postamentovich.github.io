96f0ccbb94d3f22fd9a44d500438c1f012b3f83f
postamentovich <postamentovich@gmail.com>
2020-04-10 08:30:55 +0300

    убрал jquery

--- js/main.js
+++ js/main.js
@@ -1,2 +1,100 @@
-window.addEventListener("DOMContentLoaded",()=>{const a=document.querySelector(".buttons-wrap");a.innerHTML="<button class=\"button button_yellow\" type=\"button\">\u0414\u0430</button><button class=\"button\">\u041D\u0435\u0442</button>"});
-$(".card").each(function(){$(this).hasClass("card_size_s")?$(this).css({"border-radius":"22px"}):$(this).css({"border-radius":"54px"})}),document.addEventListener("DOMContentLoaded",function(){$(".card").each(function(){$(this).hasClass("card_size_s")?$(this).css({"border-radius":"22px"}):$(this).css({"border-radius":"23px"})});var a=document.querySelector(".card.card_size_s:last-child");a.innerHTML="<div class=\"card-heading\"><div class=\"card-icon-wrap\"><img class=\"card-icon\" src=\"img/kettle.svg\"></div><h3 class=\"card-title\">\u0412\u043E\u0434\u0430 \u0432\u0441\u043A\u0438\u043F\u0435\u043B\u0430</h3> </div><div class=\"card-specs\"><p class=\"card-source\">\u0427\u0430\u0439\u043D\u0438\u043A</p><p class=\"card-time card-time_block\">16:20, \u0421\u0435\u0433\u043E\u0434\u043D\u044F</p></div>"}),document.addEventListener("DOMContentLoaded",function(){const a=document.querySelector(".buttons-wrap"),b=document.querySelector(".card_size_m:nth-child(8) .card-description");setTimeout(function(){const c=document.querySelector(".buttons-wrap .button_yellow"),d=document.createElement("div"),e=document.createElement("p"),f=document.createElement("ol"),g=document.createElement("li"),h=document.createElement("li");d.setAttribute("class","purchase-list-wrap"),e.setAttribute("class","card-description card-description_big description_critical"),e.textContent="\u0421\u043F\u0438\u0441\u043E\u043A \u043F\u043E\u043A\u0443\u043F\u043E\u043A:",f.setAttribute("class","purchase-list"),g.setAttribute("class","purchase-list__item"),g.textContent="\u0425\u043B\u0435\u0431",h.setAttribute("class","purchase-list__item"),h.textContent="\u041C\u043E\u043B\u043E\u043A\u043E",d.appendChild(e),d.appendChild(f),f.appendChild(g),f.appendChild(h),c.onclick=()=>{b.replaceWith(d),a.style.display="none"}},500),document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click",function(){document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")})},!1);
+// $(".card").each(function (e) {
+//     if ($(this).hasClass("card_size_s")) {
+//         $(this).css({ "border-radius": "22px" });
+//     } else {
+//         $(this).css({ "border-radius": "54px" });
+//     }
+// });
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((el) => {
+    if (el.classList.contains("card_size_s")) {
+        el.style.borderRadius = "22px";
+    } else {
+        el.style.borderRadius = "54px";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // $(".card").each(function (e) {
+    //     if ($(this).hasClass("card_size_s")) {
+    //         $(this).css({ "border-radius": "22px" });
+    //     } else {
+    //         $(this).css({ "border-radius": "23px" });
+    //     }
+    // });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((el) => {
+        if (el.classList.contains("card_size_s")) {
+            el.style.borderRadius = "22px";
+        } else {
+            el.style.borderRadius = "23px";
+        }
+    });
+
+    var waterContainer = document.querySelector(".card.card_size_s:last-child");
+
+    waterContainer.innerHTML =
+        '<div class="card-heading">' +
+        '<div class="card-icon-wrap">' +
+        '<img class="card-icon" src="img/kettle.svg">' +
+        "</div>" +
+        '<h3 class="card-title">Вода вскипела</h3>' +
+        " </div>" +
+        '<div class="card-specs">' +
+        '<p class="card-source">Чайник</p>' +
+        '<p class="card-time card-time_block">16:20, Сегодня</p>' +
+        "</div>";
+});
+
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        const buttonsContainer = document.querySelector(".buttons-wrap");
+        const fridgeInfoContainer = document.querySelector(
+            ".card_size_m:nth-child(8) .card-description"
+        );
+        setTimeout(function () {
+            const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
+            const purchaseListContainer = document.createElement("div");
+            const purchaseListTitle = document.createElement("p");
+            const purchaseList = document.createElement("ol");
+            const purchaseListItemOne = document.createElement("li");
+            const purchaseListItemTwo = document.createElement("li");
+
+            purchaseListContainer.setAttribute("class", "purchase-list-wrap");
+            purchaseListTitle.setAttribute(
+                "class",
+                "card-description card-description_big description_critical"
+            );
+            purchaseListTitle.textContent = "Список покупок:";
+            purchaseList.setAttribute("class", "purchase-list");
+            purchaseListItemOne.setAttribute("class", "purchase-list__item");
+            purchaseListItemOne.textContent = "Хлеб";
+            purchaseListItemTwo.setAttribute("class", "purchase-list__item");
+            purchaseListItemTwo.textContent = "Молоко";
+
+            purchaseListContainer.appendChild(purchaseListTitle);
+            purchaseListContainer.appendChild(purchaseList);
+            purchaseList.appendChild(purchaseListItemOne);
+            purchaseList.appendChild(purchaseListItemTwo);
+
+            confirmPurchaseButton.onclick = () => {
+                fridgeInfoContainer.replaceWith(purchaseListContainer);
+                buttonsContainer.style.display = "none";
+            };
+        }, 500);
+
+        document
+            .getElementsByClassName("header-menu__switcher")[0]
+            .addEventListener("click", function () {
+                document
+                    .getElementsByClassName("header-menu")[0]
+                    .classList.toggle("header-menu_active");
+            });
+    },
+    !1
+);
