277c451e47fc517dbc9cf132e909f29f3cf40303
Viacheslav Zinovev <postamentovich@gmail.com>
2020-08-15 22:18:14 +0300

    map-zones

---
+++ precache-manifest.7cc8fdc9f8500fe2d6016f531bf88b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6922775c4d45bdfbaafcc3203c26838a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9989d15bfa3291b62236",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "8ea634dcfbe49e921b01",
+    "url": "/static/css/main.a006c5e9.chunk.css"
+  },
+  {
+    "revision": "9989d15bfa3291b62236",
+    "url": "/static/js/2.f42f3ef7.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.f42f3ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea634dcfbe49e921b01",
+    "url": "/static/js/main.8b6666cd.chunk.js"
+  },
+  {
+    "revision": "7f3bf2f3bbaf2b77fffc",
+    "url": "/static/js/runtime-main.833af742.js"
+  }
+]);
